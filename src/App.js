12d391e76f6ad72ebd8e4d7aed704756672fba0a
Steven Dinhton Nguyen <stevendinhton@gmail.com>
2018-10-28 20:59:36 -0400

    made basic app to retrieve Open Weather API data

--- src/App.js
+++ src/App.js
@@ -1,25 +1,65 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
+const API = 'http://api.openweathermap.org/data/2.5/weather?units=metric&q=';
+const KEY = '4b708f1c1b3b771b1e5c408ae251188f';
+
 class App extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      selectedCity: 'Toronto',
+      data: null
+    }
+
+    this.setToronto = this.setToronto.bind(this);
+    this.setWaterloo = this.setWaterloo.bind(this);
+  }
+
+  setToronto() {
+    this.setState(
+      {selectedCity: 'Toronto'},
+      this.getWeatherData
+    );
+  }
+
+  setWaterloo() {
+    this.setState(
+      {selectedCity: 'Waterloo'},
+      this.getWeatherData
+    );
+  }
+
+  getWeatherData() {
+    fetch(API + this.state.selectedCity + '&APPID=' + KEY)
+      .then(response => response.json())
+      .then(data => this.setState({ data }));
+  }
+
+  componentDidMount() {
+    this.getWeatherData();
+  }
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <div className="app-header">
+          Current weather for {this.state.selectedCity}:
+        </div>
+        <div className="weather-container">
+          {this.state.data ? this.state.data.main.temp + '°C, ' + this.state.data.weather[0].main : ''}
+        </div>
+        <div className="btn-group btn-group-toggle" data-toggle="buttons">
+          <label className="btn btn-secondary">
+            <input type="radio" id="option1" autoComplete="off" onClick={this.setToronto}/> Toronto
+          </label>
+          <label className="btn btn-secondary">
+            <input type="radio" id="option2" autoComplete="off" onClick={this.setWaterloo}/> Waterloo
+          </label>
+        </div>
+        <div>
+          Clicking again will refresh
+        </div>
       </div>
     );
   }
